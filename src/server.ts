be8613e769378a417b332fccdfbabfe8588fa6bd
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2022-11-15 13:27:04 +0200

    fix path

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import cors from "cors";
 import bodyParser from "body-parser"
 import adminRoutes from "./routers/admin.router";
@@ -6,21 +6,26 @@ import networkRoutes from "./routers/network.route";
 import submitRoutes from "./routers/submit.route";
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-
 const port = process.env.PORT || 3000;
 
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/network', networkRoutes);
 app.use('/submit', submitRoutes);
 app.use('/admin', adminRoutes);
 
-app.get('/', function(req, res){
-    res.render('index.js');
-});
+// app.get('/network', function(req, res){
+//     res.send('Hi there, network');
+// });
+
+// app.get('/submit', function(req, res){
+//     res.send('Hi there, submit');
+// });
+
+// app.get('/admin', function(req, res){
+//     res.send('Hi there, admin');
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
